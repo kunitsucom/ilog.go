4d82f86abdbd3323ba3e78a4c6b5b9f60e6afcf2
ginokent <29125616+ginokent@users.noreply.github.com>
2023-10-22 06:15:41 +0900

    feat: add sync writer

--- global_test.go
+++ global_test.go
@@ -14,7 +14,7 @@ func TestGlobal(t *testing.T) {
 	//nolint:paralleltest,tparallel
 	t.Run("success", func(t *testing.T) {
 		buf := bytes.NewBuffer(nil)
-		defer SetGlobal(NewBuilder(DebugLevel, buf).SetTimestampZone(time.UTC).Build())()
+		defer SetGlobal(NewBuilder(DebugLevel, NewSyncWriter(buf)).SetTimestampZone(time.UTC).Build())()
 		expected := regexp.MustCompilePOSIX(`{"severity":"DEBUG","timestamp":"[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.?[0-9]*Z","caller":"ilog\.go/[a-z_]+_test\.go:[0-9]+","message":"Logf","any":"any","bool":true,"bytes":"bytes","duration":"1h1m1.001001001s","error":"unexpected EOF","eof":"EOF","float32":1\.234567,"float64":1\.23456789,"int":-1,"int32":123456789,"int64":123456789,"string":"string","time":"2023-08-13T04:38:39\.123456789\+09:00","uint":1,"uint32":123456789,"uint64":123456789}`)
 		l := Global().Copy().
 			Any("any", "any").
@@ -48,7 +48,7 @@ func TestSetStdLogger(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
 		buf := bytes.NewBuffer(nil)
 		expected := regexp.MustCompilePOSIX(`{"severity":"DEBUG","timestamp":"[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.?[0-9]*Z","caller":"ilog.go/global_test.go:[0-9]+","message":"Print\\n","any":"any","bool":true,"bytes":"bytes","duration":"1h1m1.001001001s","error":"unexpected EOF","eof":"EOF","float32":1\.234567,"float64":1\.23456789,"int":-1,"int32":123456789,"int64":123456789,"string":"string","time":"2023-08-13T04:38:39\.123456789\+09:00","uint":1,"uint32":123456789,"uint64":123456789}` + "\n")
-		l := NewBuilder(DebugLevel, buf).
+		l := NewBuilder(DebugLevel, NewSyncWriter(buf)).
 			SetTimestampZone(time.UTC).
 			Build().
 			Any("any", "any").
